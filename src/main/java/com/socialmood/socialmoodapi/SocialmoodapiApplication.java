72e5bb5d8654eda7b16044ae73be9b2cc84e0441
Diego Alves <diegoalvesdc12@gmail.com>
2025-03-03 11:57:41 -0300

    first commit

---
+++ src/main/java/com/socialmood/socialmoodapi/SocialmoodapiApplication.java
@@ -0,0 +1,13 @@
+package com.socialmood.socialmoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmoodapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialmoodapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialmood/socialmoodapi/SocialmoodapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmood.socialmoodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmoodapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
